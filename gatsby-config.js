a48d7d08402a910f75d1d34d8bedb7bf2b5b679e
Marllon Frizzo <mffrizzo@gmail.com>
2018-08-22 20:22:28 -0300

    Removendo arquivos

--- gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Marllon Frizzo',
-  },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-antd'],
-  pathPrefix: `/marllonfrizzo.github.io`,
-}
