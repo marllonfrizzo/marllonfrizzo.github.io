89a7754e35449795fc0e1aacf7e3cf16a64b20a8
Marllon Frizzo <mffrizzo@gmail.com>
2018-08-22 20:45:06 -0300

    Updates

---
+++ path----98f075016aee35e2d739.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{122:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Marllon Frizzo"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----98f075016aee35e2d739.js.map
