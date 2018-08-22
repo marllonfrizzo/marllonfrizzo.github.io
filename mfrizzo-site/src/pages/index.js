a5c06061f591aae2a2e3ace8a2219de10d692ab2
Marllon Frizzo <mffrizzo@gmail.com>
2018-08-22 20:42:47 -0300

    Arquivos iniciais

---
+++ mfrizzo-site/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Olá</h1>
+    <h3>Seja bem-vindo(a)!</h3>
+    <p>Estatísticas do Last.fm, clique <Link to="/musicas/">aqui</Link>.</p>
+  </div>
+)
+
+export default IndexPage
---
+++ mfrizzo-site/src/values/apiKeys.js
@@ -0,0 +1,3 @@
+const API_KEY = 'fe11080140eab7a1671545428dbddf4d';
+
+export default API_KEY;
