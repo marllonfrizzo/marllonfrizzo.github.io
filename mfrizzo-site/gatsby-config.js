f2a77e46dc15a3ea93f9c3f62f10a640027366bb
Marllon Frizzo <mffrizzo@gmail.com>
2018-08-30 17:29:11 -0300

    Updates

--- mfrizzo-site/gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Marllon Frizzo',
-    pathPrefix: `/marllonfrizzo.github.io`,
-  },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-antd'],
-}
--- mfrizzo-site/src/values/apiKeys.js
+++
@@ -1,3 +0,0 @@
-const API_KEY = 'fe11080140eab7a1671545428dbddf4d';
-
-export default API_KEY;
