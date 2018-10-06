a1ab1e5d27abdd7ad7a2f60fba5990521a760b7f
Marllon Frizzo <mffrizzo@gmail.com>
2018-10-05 22:53:09 -0300

    Updates

---
+++ path---index-0b4bf1b8c9ccc18e293b.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{480:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Página pessoal de Marllon Frizzo"}}},pathContext:{}}}});
+//# sourceMappingURL=path---index-0b4bf1b8c9ccc18e293b.js.map
