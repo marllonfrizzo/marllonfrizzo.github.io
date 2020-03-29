c408f22de1f8d296b67d7044ce597de8a4f48263
Marllon Frizzo <mffrizzo@gmail.com>
2020-03-29 19:36:40 -0300

    Updates

---
+++ precache-manifest.fa377c0a3339888020e9f360dabf97ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c013549e10e8e705084901ab8129cdc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12216273b2bcaffdcbe3",
+    "url": "/static/css/main.72e512b2.chunk.css"
+  },
+  {
+    "revision": "c04152ec279aa5110d48",
+    "url": "/static/js/2.00cb58c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00cb58c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12216273b2bcaffdcbe3",
+    "url": "/static/js/main.de0f5256.chunk.js"
+  },
+  {
+    "revision": "61bba36dd7b61749ccb5",
+    "url": "/static/js/runtime-main.3025841b.js"
+  }
+]);
